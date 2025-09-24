3f58136ec99614b31ba6c1afa95474d7118629a5
Brendan Graetz <bguiz@users.noreply.github.com>
2025-09-24 12:38:00 +0800

    fix: use different filenames and docker image names to avoid overlaps; also use set -e to fail fast
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

--- verify-injective-release/verify-injective-release.sh
+++ verify-injective-release/verify-injective-release.sh
@@ -2,10 +2,19 @@
 # Obtain the value for VERSION_TAG from https://github.com/InjectiveLabs/injective-chain-releases/releases
 # and replace the value below
 
-VERSION_TAG="v1.16.1-1754161770"
+VERSION_TAG="v1.16.4-1758323548"
 
-curl -JLO https://github.com/InjectiveLabs/injective-chain-releases/releases/download/${VERSION_TAG}/linux-amd64.zip
-unzip linux-amd64.zip
+set -e
 
-docker buildx build --platform=linux/x86_64 -t injective-release-verify .
-docker run --platform=linux/x86_64 injective-release-verify
+ZIP_URL="https://github.com/InjectiveLabs/injective-chain-releases/releases/download/${VERSION_TAG}/linux-amd64.zip"
+ZIP_FILE="injective-${VERSION_TAG}-linux-amd64.zip"
+DOCKER_IMG_NAME="injective-release-verify-${VERSION_TAG}"
+curl \
+  --location \
+  --output "${ZIP_FILE}" \
+   "${ZIP_URL}"
+rm -f libwasmvm.x86_64.so injectived peggo
+unzip "${ZIP_FILE}"
+
+docker buildx build --platform=linux/x86_64 -t "${DOCKER_IMG_NAME}" .
+docker run --platform=linux/x86_64 "${DOCKER_IMG_NAME}"
